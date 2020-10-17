7b5d42826234e318d695dd6c929e316bbbea6889
sam1993316 <54906199+sam1993316@users.noreply.github.com>
2020-10-17 16:02:10 -0400

    Start menu
    -placeholder button
    -working on navigation

---
+++ app/components/customer/CustomerMain.js
@@ -0,0 +1,42 @@
+import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+
+const Separator = () => (
+    <View style={styles.separator} />
+);
+
+export default function CustomerMain() {
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text>Reverse Delivery app</Text>
+                <Text>Sponsored by NCR</Text>
+            </View>
+
+            {/* <View>
+                <Button title="Customer Route" onPress={() => { Alert.alert('Customer Router pressed') }}></Button>
+
+                <Separator></Separator>
+
+                <Button title="Kitchen Route" onPress={() => { Alert.alert('Customer Router pressed') }} ></Button>
+            </View> */}
+
+            <StatusBar style="auto" />
+
+
+
+        </View>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
